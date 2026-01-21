96edb9315ef4c397ad11bca6ee2e2481f45ebaf2
Priyanshu pal <165457868+palpriyanshu94@users.noreply.github.com>
2026-01-21 21:59:25 +0530

    Time: 0 ms (100.00%) | Memory: 9 MB (93.37%) - LeetSync

---
+++ 409-longest-palindrome/longest-palindrome.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int longestPalindrome(string s) {
+         vector<int> freq(128, 0); 
+
+    for (char c : s) {
+        freq[c]++;
+    }
+
+    int length = 0;
+    bool hasOdd = false;
+
+    for (int count : freq) {
+        if (count % 2 == 0) {
+            length += count;
+        } else {
+            length += count - 1;
+            hasOdd = true;
+        }
+    }
+
+    if (hasOdd) length++;
+
+    return length;
+    }
+};
